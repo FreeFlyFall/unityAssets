c54efc46bc182bbd458d135abf8cac089576ccd8
Ryan VanSlyke <ryanjvanslyke@gmail.com>
2018-09-05 10:25:11 -0400

    Added gun and flashlight logic
    The beautiful ammo manager :))

--- Scripts/Gunplay/Gun.cs
+++ Scripts/Gunplay/Gun.cs
@@ -1,43 +1,127 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-//General gun behaviour script
-public class Gun : MonoBehaviour
-{
+public class Gun : MonoBehaviour {
+
+    public Camera fpsCam;
+    public Text AmmoDisplay;
+
     public float damage = 10f;
     public float range = 100f;
-    public Camera fpsCam;
+    
     public ParticleSystem muzzleFlash;
     public GameObject impactEffect;
     public float impactForce = 100f;
-    public float fireRate = 8f;
+
+    public float fireRate = 10f;
     public float nextTimeToFire = 0f;
 
-	void Update ()
+    public AudioSource gunSound;
+    public AudioSource emptyFire;
+    public AudioSource reload;
+
+    public int totalAmmo;
+    public int clipAmmo;
+    private bool reloadPause;
+    private readonly int clipAmmoLimit = 60;
+    private readonly int totalAmmoLimit = 600;
+
+    void Start()
     {
+        totalAmmo = totalAmmoLimit;
+        clipAmmo = clipAmmoLimit;
+    }
+
+	void Update () {
         // For automatic shots. remove second conditional, use GetButtonDown,
         // and keep only the shoot method in the conditional body for semi-auto
-        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        if(Input.GetButton("Fire1") &&
+            Time.time >= nextTimeToFire &&
+            clipAmmo > 0 &&
+            reloadPause != true)
         {
             //Since dividing, greater fire rate means less shot delay
-            nextTimeToFire = Time.time + 1f / fireRate;
+            nextTimeToFire = (Time.time + 1f / fireRate);
             Shoot();
         }
-	}
+        // Empty Fire
+        else if(Input.GetButton("Fire1") &&
+            Time.time >= nextTimeToFire &&
+            reloadPause != true)
+        {
+            nextTimeToFire = (Time.time + 1f / fireRate);
+            emptyFire.Play();
+        }
+        //Reload
+        if (Input.GetKeyDown(KeyCode.R) && totalAmmo > 0)
+        {
+            StartCoroutine(Reload());
+        }
+        // Refresh ammo display
+        AmmoDisplay.text = clipAmmo + " / " + totalAmmo;
+    }
+
+    IEnumerator Reload()
+    {
+        // Change condition to stop the dependents
+        reloadPause = true;
+        reload.Play();
+        // 3 seconds from audio clip length
+        yield return new WaitForSeconds(3);
+        reloadPause = false;
+
+        ///Reload Logic
+        // If you have any ammo
+        if(totalAmmo > 0)
+        {
+            /* Take the value of the ammo that has been used from the clip
+               And subtract it from the total ammo
+               if the total ammo is below zero, it now represents the
+               deficit of ammo for the final clip
+               Could also be written like this after
+               declaring the local variable for the used ammo:
+                // usedAmmo = clipAmmoLimit - clipAmmo;
+                // totalAmmo -= usedAmmo; */
+            totalAmmo -= clipAmmoLimit - clipAmmo;
+            // If the total ammo is above zero after accounting
+            // for the clip space, there's no deficit, so fill it to the limit
+            if(totalAmmo > 0)
+            {
+                clipAmmo = clipAmmoLimit;
+            }
+            // However, if the total ammo is below zero after accounting for the deficit,
+            // there's not enough ammo to fill the clip
+            else if(totalAmmo < 0)
+            {
+                /* Use the deficit value to establish the ammo value in the final clip
+                   Could be written like this after declaring a local variable for the deficit
+                   for ease of understanding, since it's effectively adding a negative
+                      //deficit = 0 - totalAmmo;
+                      //clipAmmo = clipAmmoLimit - deficit; */
+                clipAmmo = clipAmmoLimit + totalAmmo;
+                // Reset the totalAmmo to zero so the display doesn't show the negative value
+                totalAmmo = 0;
+            }
+        }
+    }
+
     void Shoot()
     {
         // Play muzzle flash attached to gun
         muzzleFlash.Play();
+        gunSound.Play();
+        --clipAmmo;
 
         // Create a Raycast from the camera, forwards, named hit, with the range var
         RaycastHit hit;
         if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
             // If it hits, tell what was hit, pass damage to
-            // the takeDamage method from target.cs
+            // the takeDamage method from target.cs 
             Debug.Log(hit.transform.name);
-            Target target = hit.transform.GetComponent<Target>();
+            TargetHealth target = hit.transform.GetComponent<TargetHealth>();
             if (target != null)
             {
                 target.TakeDamage(damage);
@@ -50,8 +134,8 @@ public class Gun : MonoBehaviour
             }
             // Add the impact effect where the raycast impacts, and destroy
             // the effect after 2 seconds.
-            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
-            Destroy(impactGO, 2f);
+            GameObject impactGo = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGo, 2f);
         }
     }
 }
--- Scripts/Triggers/Flashlight.cs
+++ Scripts/Triggers/Flashlight.cs
@@ -1,29 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-// In Progress
-// For setup of flashlight with sound
-// Delay for feedback(switches light bool on second click of button sound)
+/* In Progress
+    For setup of flashlight with sound,
+    intensity variation, and battery count/level */
 public class Flashlight : MonoBehaviour {
 
-    public GameObject flashlight;
-    // public float maxEnergy;
-    // private float currentEnergy;
+    public GameObject lightAsObject;
+    public float energy;
+    private float maxEnergy = 100f;
+    public int numBatteries;
     private bool isEnabled = true;
     public AudioSource ClickOn;
     public AudioSource ClickOff;
+    public AudioSource Recharge;
+    public Light light;
+    public Text batteryDisplay;
 
+
+    void Start ()
+    {
+        energy = maxEnergy;
+        numBatteries = 3;
+	}
+	
 	void Update ()
     {
-        //equip
+        // Set up Battery UI
+        batteryDisplay.text = "Batteries (Q): " + numBatteries;
+        // Turn on & off with sound
+        ToggleLight();
+        
+        // Handle battery level effects
+        /// should be replaced with animations for smooth transitions or other effects
+        if (isEnabled)
+        {
+            DrainBattery();
+            ComputeLightStatus();
+        }
+        // Handle battery reloading
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            ReloadBatteries();
+        }
+        // For manually incrementing battery level
+        //if (Input.GetKeyDown(KeyCode.I))
+        //{
+        //    ++numBatteries;
+        //}
+    }
+
+    public void ToggleLight()
+    {
         if (Input.GetKeyDown(KeyCode.F))
         {
             isEnabled = !isEnabled;
             if (isEnabled)
             {
-                StartCoroutine(WaitTimeForClick());
                 ClickOn.Play();
+                StartCoroutine(WaitTimeForClick());
             }
             else
             {
@@ -31,18 +68,55 @@ public class Flashlight : MonoBehaviour {
                 StartCoroutine(WaitTimeForClick());
             }
         }
-	}
+    }
+
+    public void DrainBattery()
+    {
+        if (energy > 0)
+        {
+            energy -= Time.deltaTime / 3f;
+        }
+    }
+
+    public void ComputeLightStatus()
+    {
+        if (energy >= 50f)
+        {
+            light.intensity = 1.5f;
+            light.range = 150f;
+        } 
+        else if (energy >= 25f)
+        {
+            light.intensity = 1f;
+            light.range = 105f;
+        }
+        else if (energy >= 0f)
+        {
+            light.intensity = 0.5f;
+            light.range = 50f;
+        }
+    }
+
+    public void ReloadBatteries()
+    {
+        if (energy <= 50 && numBatteries > 0)
+        {
+            Recharge.Play();
+            --numBatteries;
+            energy = 100f;
+        }
+    }
 
     IEnumerator WaitTimeForClick()
     {
         yield return new WaitForSeconds(0.35f);
         if (isEnabled)
         {
-            flashlight.SetActive(true);
+            lightAsObject.SetActive(true);
         }
         else
         {
-            flashlight.SetActive(false);
+            lightAsObject.SetActive(false);
         }
     }
 }
